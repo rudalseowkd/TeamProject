cab11007f58b56697cd626ae95d026a8e89d503d
ahahaha54321 <ahahaha54321@naver.com>
2020-05-24 23:26:12 +0900

    검색기 중간까지 구현... 잘 안된다

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -37,11 +37,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("???", "onCreate: ???????????????????????????????????????1111");
 
         Toolbar myToolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        Log.d("???", "onCreate: ???????????????????????????????????????2222");
+
         setContentView(R.layout.activity_main);
 
 
@@ -57,51 +56,75 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
         getMenuInflater().inflate(R.menu.my_menu, menu);
-
-        Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
-
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setQueryHint("영화제목을 입력하세요.");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
-
-            //검색어를 다 입력하고 서치 버튼을 눌렀을때
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                Toast.makeText(MainActivity.this, s + "에 대한 영화를 검색합니다.", Toast.LENGTH_LONG).show();
-                //여기서 AsyncTask를 이용 검색 리퀘스트로 데이터를 받아 오게 처리 하자. - AsyncTask 공유할것.
-                return false;
-            }
-
-            //검색 입력창에 새로운 텍스트가 들어갈때 마다 호출 - 여기선 필요 없음
-            @Override
-            public boolean onQueryTextChange(String s) {
-                //Log.d("Search", "keyword: " + s);
-                return false;
-            }
-        });
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch(id){
-            case R.id.action_search:
-                //Toast.makeText(this, "action_search", Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(this, "앱설정-준비중", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                Toast.makeText(this, "default", Toast.LENGTH_LONG).show();
-                return super.onOptionsItemSelected(item);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
+//        getMenuInflater().inflate(R.menu.my_menu, menu);
+//
+//        Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
+//
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) searchItem.getActionView();
+//        searchView.setQueryHint("영화제목을 입력하세요.");
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+//
+//            //검색어를 다 입력하고 서치 버튼을 눌렀을때
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                Toast.makeText(MainActivity.this, s + "에 대한 영화를 검색합니다.", Toast.LENGTH_LONG).show();
+//                //여기서 AsyncTask를 이용 검색 리퀘스트로 데이터를 받아 오게 처리 하자. - AsyncTask 공유할것.
+//                return false;
+//            }
+//
+//            //검색 입력창에 새로운 텍스트가 들어갈때 마다 호출 - 여기선 필요 없음
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                //Log.d("Search", "keyword: " + s);
+//                return false;
+//            }
+//        });
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        switch(id){
+//            case R.id.action_search:
+//                //Toast.makeText(this, "action_search", Toast.LENGTH_LONG).show();
+//                return true;
+//            case R.id.action_settings:
+//                Toast.makeText(this, "앱설정-준비중", Toast.LENGTH_LONG).show();
+//                return true;
+//            default:
+//                Toast.makeText(this, "default", Toast.LENGTH_LONG).show();
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 
 
