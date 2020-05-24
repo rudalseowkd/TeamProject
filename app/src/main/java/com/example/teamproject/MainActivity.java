70d47e0da0b062376d45ae21b4b18b485ed0181e
ahahaha54321 <ahahaha54321@naver.com>
2020-05-24 22:27:58 +0900

    검색기 중간까지 구현... 잘 안된다

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.teamproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,6 +26,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -27,19 +35,75 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Movie> movieList;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        Log.d("pls", "onCreateOptionsMenu: ??????????????????????????????????????????????????????????????????");
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("영화제목을 입력하세요.");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            //검색어를 다 입력하고 서치 버튼을 눌렀을때
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(MainActivity.this, s + "에 대한 영화를 검색합니다.", Toast.LENGTH_LONG).show();
+                //여기서 AsyncTask를 이용 검색 리퀘스트로 데이터를 받아 오게 처리 하자. - AsyncTask 공유할것.
+                return false;
+            }
+
+            //검색 입력창에 새로운 텍스트가 들어갈때 마다 호출 - 여기선 필요 없음
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //Log.d("Search", "keyword: " + s);
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_search:
+                //Toast.makeText(this, "action_search", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "앱설정-준비중", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                Toast.makeText(this, "default", Toast.LENGTH_LONG).show();
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("???", "onCreate: ???????????????????????????????????????1111");
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        Log.d("???", "onCreate: ???????????????????????????????????????2222");
         setContentView(R.layout.activity_main);
+
 
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         movieList = new ArrayList<Movie>();
-
 
         AsyTask asyncTask = new AsyTask();
         asyncTask.execute();
 
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,2));
     }
+
+
 
     public class AsyTask extends AsyncTask<String, Void, Movie[]>{
 
@@ -86,5 +150,11 @@ public class MainActivity extends AppCompatActivity {
             adapter = new RecycleAdp(MainActivity.this,movieList);
             recyclerView.setAdapter(adapter);
         }
+
+
+
     }
+
+
+
 }
